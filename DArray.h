475a913989717e81e93c3592f92bf07c0024731a
SamEducational <89948811+SamEducational@users.noreply.github.com>
2022-07-05 19:51:54 -0700

    Add files via upload

---
+++ DArray.h
@@ -0,0 +1,56 @@
+/*
+	Truong, Sam
+
+	CS A200
+	August 8, 2018
+
+	Lab 1
+*/
+
+#ifndef DARRAY_H
+#define DARRAY_H
+
+#include <iostream>
+using namespace std;
+
+const int CAP = 50;
+
+class DArray
+{
+	friend ostream& operator<<(ostream& out, const DArray& theArray);
+	// Overloaded insertion operator to print array
+
+public:
+    DArray( );
+	DArray(const DArray&);
+    DArray(int newCapacity);
+
+    void addElement(int newElement);
+
+	int getNumOfElements() const;
+
+	DArray& operator=(const DArray&);
+	
+	~DArray();
+
+	// Declaration function emptyArray
+	void emptyArray();
+
+	// Declaration function append
+	void append(const DArray& a);
+
+
+	// Declaration move constructor
+
+
+	// Declaration move assignment operator
+
+
+private:
+    int *a;					//will point to an array of integers
+    int capacity;			//capacity of the array
+    int numOfElements;		//total number of elements in the array
+
+};
+
+#endif
---
+++ Functions.cpp
@@ -0,0 +1,41 @@
+/*
+	Truong, Sam
+
+	CS A200
+	August 8, 2018
+
+	Lab 1
+*/
+
+#include "DArray.h"
+
+// Definition function emptyArray
+void DArray::emptyArray() {
+	numOfElements = 0;
+}
+
+// Definition function appendArray
+void DArray::append(const DArray& otherArray) {
+	if (otherArray.numOfElements != 0) {
+		if (capacity < numOfElements+otherArray.numOfElements) {
+			delete[] a;
+			a = nullptr;
+
+		}
+		else {
+			/*for (int i = 0; i < otherArray.numOfElements; ++i) {
+				a[numOfElements] = otherArray.a[i];
+				++numOfElements;
+			}*/
+		}
+	}
+}
+
+
+// Definition move constructor
+
+
+// Definition move assignment operator
+
+
+
